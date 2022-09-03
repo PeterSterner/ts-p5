cd540b4c6d49ff245bcc8ddf55d09bf5d1c67dcf
petey33 <psterner@gmail.com>
2022-09-03 16:13:06 +0200

    mousePressed virker nu.

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 import "p5";
 
+window.mousePressed = () => {
+  fill("blue");
+  ellipse(mouseX, mouseY, 50, 50);
+};
+
 window.setup = () => {
   createCanvas(windowWidth, windowHeight);
   background(220);
@@ -7,6 +12,10 @@ window.setup = () => {
 
 window.draw = () => {
   circle(windowWidth / 2, windowHeight / 2, 50);
+  fill("red");
+  if (mouseIsPressed) {
+    circle(mouseX, mouseY, 40);
+  }
 };
 
 function helloWorld(name: string) {
