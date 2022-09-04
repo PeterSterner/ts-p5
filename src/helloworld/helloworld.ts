7ae1029e6f890ac373939c08720098c33b61b53c
petey33 <psterner@gmail.com>
2022-09-04 15:27:47 +0200

    Mindre rettelse.

--- src/helloworld/helloworld.ts
+++ src/helloworld/helloworld.ts
@@ -1,13 +1,5 @@
 /*
 Et eksempel på typescript uden brug af p5.
-
-For at køre koden nedenfor i VSCode gør da følgende:
-
-1) Ctrl+Shift+B.
-2) Vælg tsc: watch - tsconfig.json
-3) 
-
-
 */
 
 function helloWorld(name: string) {
