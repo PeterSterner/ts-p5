5dff3b48e65eb8067d4ff05aeb5fbc6823db577b
petey33 <psterner@gmail.com>
2022-09-04 15:33:44 +0200

    En mindre rettelse.

--- src/helloworld/helloworld.ts
+++ src/helloworld/helloworld.ts
@@ -2,9 +2,8 @@
 Et eksempel på typescript uden brug af p5.
 */
 
-function helloWorld(name: string) {
+function helloWorld(name: string):void {
     console.log("From " + name + ": Hello world!");
-    console.log(window.mousePressed);
 }
 
 helloWorld("Peter");
